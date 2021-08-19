95498c6b33d665cc56583c7a765fe7168f9bb2b7
Rostislav <qasik228@yandex.ru>
2021-08-19 21:53:59 +0700

    mod Make brain package cli add eslintrc.uml

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
-export const name = readlineSync.question('May I have your name? ');
+const name = readlineSync.question('May I have your name? ');
+export default name;
