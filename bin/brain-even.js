390958287c0540f0a999ea5c5b936cfb4721ccf8
Rostislav <qasik228@yandex.ru>
2021-08-21 13:21:43 +0700

    step 5. mod bin/brain-even.js, src/gamescode/even.js, src/random.js. add src/even-data.js

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startEven from '../src/even-data.js';
+
+startEven();
--- src/gamescode/even.js
+++ src/gamescode/even.js
@@ -1,6 +1,28 @@
 import readlineSync from 'readline-sync';
 
-const round = 3;
-const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+const round = 3; // Кол-во раундов
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".'; // Правила
 
-
+// Код игры
+const evenGame = (gameData) => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello ${name}!`);
+    console.log(rule);
+    // Начало цикла
+    for (let i = 1; i<=round; i++) {
+        const [quest, correct] = gameData();
+        console.log(`Question: ${quest}`);
+        const answer = readlineSync.question('Your answer: ');
+        // Проверка ответа игрока
+        if (answer === correct) {
+            console.log('Correct!'); // Харошь
+        } else {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'`); // Shit happens
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+    }
+    console.log(`Congratulations, ${name}!`) // OMG MLG
+};
+export default evenGame;
