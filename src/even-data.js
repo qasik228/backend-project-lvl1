2012e8680c018295d413767dd26d085b11d16fb0
Rostislav <qasik228@yandex.ru>
2021-08-21 13:56:23 +0700

    step 5. mod bin/brain-even.js, package.json, src/even-data.js, src/gamescode/even.js, src/random.js

--- src/even-data.js
+++ src/even-data.js
@@ -2,13 +2,13 @@ import getRandomInRange from './random.js';
 import evenGame from './gamescode/even.js';
 
 const gameData = () => {
-    const quest = getRandomInRange(1, 100);
-    const correct = quest % 2 === 0 ? 'yes' : 'no';
-    return [quest, correct];
+  const quest = getRandomInRange(1, 100);
+  const correct = quest % 2 === 0 ? 'yes' : 'no';
+  return [quest, correct];
 };
 
 const startEven = () => {
-    evenGame(gameData);
+  evenGame(gameData);
 };
 
 export default startEven;
--- src/gamescode/even.js
+++ src/gamescode/even.js
@@ -5,24 +5,24 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".'; // П
 
 // Код игры
 const evenGame = (gameData) => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello ${name}!`);
-    console.log(rule);
-    // Начало цикла
-    for (let i = 1; i<=round; i++) {
-        const [quest, correct] = gameData();
-        console.log(`Question: ${quest}`);
-        const answer = readlineSync.question('Your answer: ');
-        // Проверка ответа игрока
-        if (answer === correct) {
-            console.log('Correct!'); // Харошь
-        } else {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'`); // Shit happens
-            console.log(`Let's try again, ${name}!`);
-            return;
-        }
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${name}!`);
+  console.log(rule);
+  // Начало цикла
+  for (let i = 1; i <= round; i += 1) {
+    const [quest, correct] = gameData();
+    console.log(`Question: ${quest}`);
+    const answer = readlineSync.question('Your answer: ');
+    // Проверка ответа игрока
+    if (answer === correct) {
+      console.log('Correct!'); // Харошь
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'`); // Shit happens
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
-    console.log(`Congratulations, ${name}!`) // OMG MLG
+  }
+  console.log(`Congratulations, ${name}!`); // OMG MLG
 };
 export default evenGame;
