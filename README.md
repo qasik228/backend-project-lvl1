abbfa99255d546d13f6f7ab9d38becaa31e16171
Rostislav <qasik228@yandex.ru>
2021-08-22 16:44:20 +0700

    step 6. mod README.md, bin/brain-calc.js, package.json, src/gamescode/calc-data.js

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 [![link](https://github.com/qasik228/backend-project-lvl1/actions/workflows/lint.yml/badge.svg)](https://github.com/qasik228/backend-project-lvl1/actions/workflows/link.yml)
 
-Game - brain-even
+Game
+brain-even
 [![asciicast](https://asciinema.org/a/wWct48M2Ye7dyVNI6x8Vi7b5G.svg)](https://asciinema.org/a/wWct48M2Ye7dyVNI6x8Vi7b5G)
+
+Game
+brain-calc
+[![asciicast](https://asciinema.org/a/7qLw80tGc9W5gZh3whQbNiFb8.svg)](https://asciinema.org/a/7qLw80tGc9W5gZh3whQbNiFb8)
--- src/gamescode/calc-data.js
+++ src/gamescode/calc-data.js
@@ -5,28 +5,26 @@ const rule = 'What is the result of the expression?'; // Правила
 const sign = ['+', '-', '*'];
 
 const answer = (num1, num2, symbol) => {
-    if (symbol === '+') {
-        return String(num1+num2);
-    }
-    if (symbol === '-') {
-        return String(num1-num2);
-    }
-    if (symbol === '*') {
-        return String(num1*num2);
-    }
+  if (symbol === '+') {
+    return num1 + num2;
+  }
+  if (symbol === '-') {
+    return num1 - num2;
+  }
+  return num1 * num2;
 };
 
 const gameData = () => {
-    const num1 = getRandomInRange(1, 100);
-    const num2 = getRandomInRange(1, 100);
-    const symbol = sign[getRandomInRange(0, sign.length-1)];
-    const correct = answer(num1, num2, symbol);
-    const quest = `${num1} ${symbol} ${num2}`;
-    return [quest, correct];
+  const num1 = getRandomInRange(1, 100);
+  const num2 = getRandomInRange(1, 100);
+  const symbol = sign[getRandomInRange(0, sign.length - 1)];
+  const correct = String(answer(num1, num2, symbol));
+  const quest = `${num1} ${symbol} ${num2}`;
+  return [quest, correct];
 };
 
 const startCalc = () => {
-    Game(gameData, rule);
-  };
-  
-  export default startCalc;
+  Game(gameData, rule);
+};
+
+export default startCalc;
