35bcbb575ee2e50ea96614a259e46b961c79f6b4
Rostislav <qasik228@yandex.ru>
2021-08-24 21:22:24 +0700

    step final2

--- src/gamescode/calc-data.js
+++ src/gamescode/calc-data.js
@@ -17,7 +17,7 @@ const answer = (num1, num2, symbol) => {
 const gameData = () => {
   const num1 = getRandomInRange(1, 100); // число 1
   const num2 = getRandomInRange(1, 100); // число 2
-  const symbol = sign[getRandomInRange(0, sign.length - 1)]; // выбор случайной арифметической операции
+  const symbol = sign[getRandomInRange(0, sign.length - 1)]; // случайной арифметической операции
   const correct = String(answer(num1, num2, symbol)); // верный ответ
   const quest = `${num1} ${symbol} ${num2}`; // задача
   return [quest, correct];
